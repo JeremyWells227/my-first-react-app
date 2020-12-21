45c033f27dc3ee9733f5375f35cbc68034081750
Jeremy Wells <jswe227@gmail.com>
2020-12-21 11:11:34 -0500

    Modified for tutorial

---
+++ src/components/Overview.js
@@ -0,0 +1,13 @@
+import React, {Component,useState} from 'react'
+
+export default class Overview extends Component {
+		constructor(props) {
+			super(props) 
+		}
+		render() {
+			return(
+				<div>
+				</div>
+			)
+		}
+	} 
